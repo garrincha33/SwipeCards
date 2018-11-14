e20ec86650bc1a0a5d91cedadf651515e30a1571
Richard Price <mrspondooli@gmail.com>
2018-11-14 10:12:46 +0000

    Initial Commit

---
+++ SwipeCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeCards
+//
+//  Created by Richard Price on 14/11/2018.
+//  Copyright © 2018 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
