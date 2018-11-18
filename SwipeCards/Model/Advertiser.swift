ad1f35b145334b85f216ef1b3bd81db5c99f4a6b
Richard Price <mrspondooli@gmail.com>
2018-11-18 11:20:10 +0000

    added more cards, ability to click on cards and move to next one, with barstackview as bar indictator

--- SwipeCards/Model/Advertiser.swift
+++ SwipeCards/Model/Advertiser.swift
@@ -17,7 +17,7 @@ struct Advertiser: ProducesCardViewModel {
     func toCardViewModel() -> CardViewModel {
         let attributedString = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
         attributedString.append(NSAttributedString(string: "\n" + brandName, attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .semibold)]))
-        return CardViewModel(imageName: posterName, attributedString: attributedString, textAlignment: .center)
+        return CardViewModel(imageNames: [posterName], attributedString: attributedString, textAlignment: .center)
     }
 }
 
--- SwipeCards/Model/User.swift
+++ SwipeCards/Model/User.swift
@@ -13,13 +13,13 @@ struct User: ProducesCardViewModel {
     let name: String
     let age: Int
     let profression: String
-    let imageName: String
+    let imageNames: [String]
  
     func toCardViewModel() -> CardViewModel {
         let attributedText = NSMutableAttributedString(string: name, attributes: [.font: UIFont.systemFont(ofSize: 32, weight: .heavy)])
         attributedText.append(NSAttributedString(string: " \(age)", attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .regular)]))
         attributedText.append(NSAttributedString(string: "\n \(profression)", attributes: [.font: UIFont.systemFont(ofSize: 20, weight: .regular)]))
-        return CardViewModel(imageName: imageName, attributedString: attributedText, textAlignment: .left)
+        return CardViewModel(imageNames: imageNames, attributedString: attributedText, textAlignment: .left)
     }
     
 }
